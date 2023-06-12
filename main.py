1c21fa7ff90d43cbd2101b9728fc4a02ac6498ec
Damiano D'Antuono <damiano.dantuono@lastminute.com>
2023-06-12 18:37:01 +0200

    only print

--- main.py
+++ main.py
@@ -14,31 +14,31 @@ def file_processing(cloud_event):
 
     print("Triggered by a change in bucket: {}, file: {}".format(bucket, name))
 
-    client = bigquery.Client()
-
-    with open("sql_scripts/load_to_ext.sql.sql", "r") as f:
-        load_to_ext_query = f.read()
-
-    client.query(load_to_ext_query)
-
-    print("loaded to ext table")
-
-    with open("sql_scripts/update_dim_city.sql", "r") as f:
-        update_dim_city_query = f.read()
-
-    client.query(update_dim_city_query)
-
-    print("updated dim city table")
-
-    with open("sql_scripts/insert_into_fct.sql", "r") as f:
-        insert_into_fct_query = f.read()
-
-    client.query(insert_into_fct_query)
-
-    print("inserted into fct table")
-
-    client = storage.Client()
-    bucket = client.get_bucket(bucket)
-    blob = bucket.blob(name)
-    _ = bucket.copy_blob(blob, DESTINATION_BUCKET, name)
-    blob.delete()
+    # client = bigquery.Client()
+    #
+    # with open("sql_scripts/load_to_ext.sql.sql", "r") as f:
+    #     load_to_ext_query = f.read()
+    #
+    # client.query(load_to_ext_query)
+    #
+    # print("loaded to ext table")
+    #
+    # with open("sql_scripts/update_dim_city.sql", "r") as f:
+    #     update_dim_city_query = f.read()
+    #
+    # client.query(update_dim_city_query)
+    #
+    # print("updated dim city table")
+    #
+    # with open("sql_scripts/insert_into_fct.sql", "r") as f:
+    #     insert_into_fct_query = f.read()
+    #
+    # client.query(insert_into_fct_query)
+    #
+    # print("inserted into fct table")
+    #
+    # client = storage.Client()
+    # bucket = client.get_bucket(bucket)
+    # blob = bucket.blob(name)
+    # _ = bucket.copy_blob(blob, DESTINATION_BUCKET, name)
+    # blob.delete()
