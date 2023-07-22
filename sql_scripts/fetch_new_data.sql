a0d7d153c739c34f7880159772fbaf531e2f5665
Damiano D'Antuono <damianodantuono95@gmail.com>
2023-07-22 13:03:51 +0200

    fixed query

--- sql_scripts/fetch_new_data.sql
+++ sql_scripts/fetch_new_data.sql
@@ -3,7 +3,7 @@ FROM `real-estate-analyser-dv.dwhre.FCT_HOUSES` f
 inner join `real-estate-analyser-dv.dwhre.DIM_CITY` d on d.city_id = f.city_id
 where f.INSERT_UPDATE_TIMESTAMP >= COALESCE((SELECT last_run_start FROM `real-estate-analyser-dv.config.notifier_runner`), '1900-01-01 00:00:00')
 and not deleted_flag
-and (price = -1) or (price between 0 and 1000)
+and price <= 1000
 and city_name = 'como'
 and IS_RENT
 ;
